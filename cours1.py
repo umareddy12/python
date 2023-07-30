abf05a92555fdd0b890ea1857ec956789649290f
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-07-30 22:24:06 +0530

    Add files via upload

---
+++ cours1.py
@@ -0,0 +1,8 @@
+//using functions
+def area_triangle(base,heigth):
+    return base*heigth/2
+
+area_a=area_triangle(5,4)
+area_b=area_triangle(7,3)
+sum=area_a+area_b
+print("The sum of both area is :"+str(sum))
