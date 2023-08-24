ec7809b1ee7a9b707fdc74fb1555fb5e640bc920
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-08-24 22:29:39 +0530

    Add files via upload

---
+++ operators1.py
@@ -0,0 +1,11 @@
+# declaring
+a=8;
+b=3;
+c=6;
+d=9;
+print("a+b=",(a+b));#+(Addition)
+print("a-b=",(a-b));# - (Subtraction)
+print("c/d=",(c/d));# / (Division)
+print("c//d=",(c//d));# // (Integer Division)
+print("b%c=",(b%d));# % (Modulus)
+print("a*c=",(a*c));# * (Multiplication)
