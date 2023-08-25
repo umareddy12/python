18443b4118428f71dca6869bbbc4c1215bb50e49
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-08-25 23:22:03 +0530

    Add files via upload

---
+++ last_digit.py
@@ -0,0 +1,8 @@
+#the method solve that takes an integer as a parameter and returns the last digit of the #integer.
+def solve(num):
+    # write your code from here
+    num1=str(num)
+    num2=num1[-1]
+    return num2
+result=solve(98765)
+print(result)
