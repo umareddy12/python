6caedff50d14c71a10b3b4ca287329650e1bd7db
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-18 16:01:56 +0530

    map1fun.py

---
+++ map1fun.py
@@ -0,0 +1,6 @@
+#How to accept multiple integers on separate lines
+
+A, B = map(int,input().split())
+C, D, E = map(int, input().split())
+F, G, H, I = map(int,input().split())
+print(A, B, C, D, E, F, G, H, I)
