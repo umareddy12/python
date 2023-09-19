426b7e624cf6c1fd6ffe57ac25e845a5843ddfe4
Reddy Uma Devi <121800119+umareddy12@users.noreply.github.com>
2023-09-19 18:34:52 +0530

    negint.py

---
+++ negint.py
@@ -0,0 +1,10 @@
+#Number mirror - Negative integer
+
+#accept the count of test cases given in the the 1st line
+t = int(input())        
+#run a loop to accept 't' inputs
+for i in range(t):      
+    #accept 1 integer on the 1st line of each test case
+    N = int(input())        
+    #output the negative integer - i.e. (-N)
+    print(-N)
